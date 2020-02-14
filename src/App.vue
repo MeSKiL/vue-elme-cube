fbd153d82b3683d2dffafcc8691377b0bb52756b
meskil <403797970@qq.com>
2020-02-14 16:02:56 +0800

    goods

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,55 @@
 <template>
   <div id="app">
     <v-header :seller="seller"/>
-    <header-detail :seller="seller" />
+    <div class="tab-wrapper">
+      <tab :tabs="tabs" />
+    </div>
   </div>
 </template>
 
 <script>
   import VHeader from 'components/v-header/v-header'
-  import HeaderDetail from 'components/header-detail/header-detail'
+  import Goods from 'components/goods/goods'
+  import Ratings from 'components/ratings/ratings'
+  import Seller from 'components/seller/seller'
+  import Tab from 'components/tab/tab'
   import { getSeller } from 'api/index'
 
   export default {
     name: 'app',
     components: {
-      HeaderDetail,
-      VHeader
+      VHeader,
+      Tab
     },
     created () {
       this._getSeller()
     },
-    data() {
+    data () {
       return {
         seller: {}
       }
     },
+    computed: {
+      tabs () {
+        return [
+          {
+            label: '商品',
+            component: Goods,
+            data: { seller: this.seller }
+          }, {
+            label: '评价',
+            component: Ratings,
+            data: { seller: this.seller }
+          }, {
+            label: '商家',
+            component: Seller,
+            data: { seller: this.seller }
+          }
+        ]
+      }
+    },
     methods: {
-      _getSeller() {
+      _getSeller () {
         getSeller().then(seller => {
           this.seller = seller
         })
@@ -35,5 +59,11 @@
 </script>
 
 <style lang="stylus">
-
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
